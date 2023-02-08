fbe534c459680d97b3602f7cfc9de0cead46d18e
soucanabisdev <118570727+soucanabisdev@users.noreply.github.com>
2023-02-08 15:29:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const axios = require('axios')
 const app = express();
-//const {utcToZonedTime, format } = require('date-fns-tz')
-const timeZone = 'America/Sao_Paulo'
-var zonedDate = ""
-const pattern = 'd.M.yyyy HH:mm:ss'
-var datetime = ""
-
 var phaseId = []
 var info = []
-var infos = []
 var api = []
 var date = ""
 
@@ -21,72 +14,14 @@ app.use(express.urlencoded({
 }));
 
 
-
-app.get('/api', async (req, res) => {
-  res.send(api)
-})
-
-app.post('/pipe-associados', async (req, res) => {
-date = new Date()  
-//zonedDate =  utcToZonedTime(date, timeZone)
-//datetime = format(zonedDate, pattern)
-info = req.body.data
-
-console.log("[Pipe Associados] "+info.action+" ("+info.card.title+") de "+info.from.name+" para "+info.to.name+" por "+info.moved_by.name+" - ")
-
-infos = {
-  "pipe":"sou-associados",
-  "action":info.action,
-  "cardTitle":info.card.title,
-  "lastPhase":info.from.name,
-  "phase":info.to.name,
-  "moved":info.moved_by.name,
-  "datetime":datetime
-}
-
-api.push(infos)
-
-phaseId = req.body.data.to.id
-
-if(phaseId == "316891688"){
-
-  const options = {
-  method: "POST",
-  headers:{"Content-Type": "application/json"},
-  mode: "cors",
-  data: req.body.data,
-  url: "https://eot9ant5nl3yh5q.m.pipedream.net"
-  }
-
-  await axios(options)   
-}
-
-res.status(200).end()
-
-return api
-
-});
-
-
 app.post('/pipe-pedidos', async (req, res) => {
 
-  date = new Date()  
-  zonedDate =  utcToZonedTime(date, timeZone)
-  datetime = format(zonedDate, pattern)
+  date = new Date()
+
   info = req.body.data
+  api.push(info)
 
-  infos = {
-    "pipe":"sou-pedidos",
-    "action":info.action,
-    "cardTitle":info.card.title,
-    "lastPhase":info.from.name,
-    "phase":info.to.name,
-    "moved":info.moved_by.name,
-    "datetime":datetime
-  }
-
-  api.push(infos)
-  console.log("[Pipe Pedidos]"+info.action+" ("+info.card.title+") de "+info.from.name+" para "+info.to.name+" por "+info.moved_by.name+" - "+datetime)
+  console.log("[Pipe Pedidos]"+info.action+" de "+info.from.name+" para "+info.to.name+" por "+info.moved_by.name+" - "+date)
 
 
   phaseId = req.body.data.to.id
@@ -116,37 +51,57 @@ app.post('/pipe-pedidos', async (req, res) => {
     url: "https://eownrtbxf42g5iq.m.pipedream.net"
     }
 
-    await axios(options)   
-  }
+    await axios(options)
 
-  res.status(200).end()
+    res.status(200).end()
+  }
 
   return api
 
 });
 
+app.get('/api', async (req, res) => {
+    res.send(api)
+})
 
-app.post('/pipe-servicos', async (req, res) => {
-  date = new Date()  
-  zonedDate =  utcToZonedTime(date, timeZone)
-  datetime = format(zonedDate, pattern)
+app.post('/pipe-associados', async (req, res) => {
+  date = new Date()
+  
+  
   info = req.body.data
-  info.push(datetime)
+  api.push(info)
 
-  infos = {
-    "pipe":"sou-servicos",
-    "action":info.action,
-    "cardTitle":info.card.title,
-    "lastPhase":info.from.name,
-    "phase":info.to.name,
-    "moved":info.moved_by.name,
-    "datetime":datetime
+  console.log("[Pipe Associados]"+info.action+" de "+info.from.name+" para "+info.to.name+" por "+info.moved_by.name+" - "+date)
+
+
+  phaseId = req.body.data.to.id
+
+  if(phaseId == "316891688"){
+
+    const options = {
+    method: "POST",
+    headers:{"Content-Type": "application/json"},
+    mode: "cors",
+    data: req.body.data,
+    url: "https://eot9ant5nl3yh5q.m.pipedream.net"
+    }
+
+    await axios(options)
+
+    res.status(200).end()
   }
 
+  return api
 
-  api.push(infos)
+});
 
-  console.log("[Pipe Serviços] "+info.action+" de "+info.from.name+" para "+info.to.name+" por "+info.moved_by.name+" - "+datetime)
+app.post('/pipe-servicos', async (req, res) => {
+  date = new Date()
+
+  info = req.body.data
+  api.push(info)
+
+   console.log("[Pipe Serviços]"+info.action+" de "+info.from.name+" para "+info.to.name+" por "+info.moved_by.name+" - "+date)
 
   phaseId = req.body.data.to.id
 
@@ -161,34 +116,21 @@ app.post('/pipe-servicos', async (req, res) => {
     }
 
     await axios(options)
-  }
 
-  res.status(200).end()
+    res.status(200).end()
+  }
 
   return api
 
 });
 
-
 app.post('/pipe-liga', async (req, res) => {
-  date = new Date()  
-  zonedDate =  utcToZonedTime(date, timeZone)
-  datetime = format(zonedDate, pattern)
+  date = new Date()
+
   info = req.body.data
+  api.push(info)
 
-  infos = {
-    "pipe":"pipe-liga",
-    "action":info.action,
-    "cardTitle":info.card.title,
-    "lastPhase":info.from.name,
-    "phase":info.to.name,
-    "moved":info.moved_by.name,
-    "datetime":datetime
-  }
-
-  api.push(infos)
-
-  console.log("[Pipe Liga] "+info.action+" de "+info.from.name+" para "+info.to.name+" por "+info.moved_by.name+" - "+datetime)
+   console.log(info.action+" de "+info.from.name+" para "+info.to.name+" por "+info.moved_by.name+" - "+date)
 
   phaseId = req.body.data.to.id
 
@@ -203,6 +145,8 @@ app.post('/pipe-liga', async (req, res) => {
     }
 
     await axios(options)
+
+    res.status(200).end()
   }
 
   if(phaseId == "315282523"){ // pedido enviado
@@ -215,6 +159,8 @@ app.post('/pipe-liga', async (req, res) => {
     }
 
     await axios(options)
+
+    res.status(200).end()
   }
 
   if(phaseId == "315282527"){ // msg whatsapp
@@ -228,56 +174,14 @@ app.post('/pipe-liga', async (req, res) => {
 
     await axios(options)
 
+    res.status(200).end()
   }
 
-  res.status(200).end()
   return api
 
 });
 
-app.post('/pipe-comunicacao', async (req, res) => {
-  date = new Date()  
-  zonedDate =  utcToZonedTime(date, timeZone)
-  datetime = format(zonedDate, pattern)
-  info = req.body.data
-  
-  console.log("[Pipe Comunicação] "+info.action+" ("+info.card.title+") de "+info.from.name+" para "+info.to.name+" por "+info.moved_by.name+" - "+datetime)
-  
-  infos = {
-    "pipe":"sou-comunicacao",
-    "action":info.action,
-    "cardTitle":info.card.title,
-    "lastPhase":info.from.name,
-    "phase":info.to.name,
-    "moved":info.moved_by.name,
-    "datetime":datetime
-  }
-  
-  api.push(infos)
-  
-  phaseId = req.body.data.to.id
-  
-  if(phaseId == "318409148"){
-  
-    const options = {
-    method: "POST",
-    headers:{"Content-Type": "application/json"},
-    mode: "cors",
-    data: req.body.data,
-    url: "https://eo9hfgd2rdx1cre.m.pipedream.net"
-    }
-  
-    await axios(options)   
-  }
-  
-  res.status(200).end()
-  
-  return api
-  
-  });
-
-
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Webhooks Pipefy Api Run!');
+    console.log('listening on *:3000');
   });
   
