5b541a3d7afb6bdb66f0f74fbf28a1fb01e8455e
soucanabisdev <118570727+soucanabisdev@users.noreply.github.com>
2023-02-13 17:45:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -207,7 +207,37 @@ app.post('/pipe-juridico', async (req, res) => {
   
   phaseId = req.body.data.to.id
   
-  if(phaseId == "311533750"){
+  optionsPipe = {
+        method: 'POST',
+        url : 'https://api.pipefy.com/graphql',
+        headers: {
+            'accept': 'application/json',
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDE5OTU2ODEsImVtYWlsIjoiZmVsaXBlcm9zZW5la0BnbWFpbC5jb20iLCJhcHBsaWNhdGlvbiI6MzAwMTQyMDIwfX0.JugAF92MqbUV_fLVKEcF5jUI3G4G2hlAmLeBJ-dEfsEIlX3gdKO1IfbQRUYvHvAk569vuD9K_zCrKylY6R6agw',
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify({
+            query: '{card(id: 646034939) {fields{name value }}}'
+        })
+    };
+
+    await axios(optionsPipe)
+      .then(function (response) {
+        data = response.data.data.card.fields
+        return data
+    })
+    .catch(function (error) {
+       console.log(error);
+    })
+
+    data.forEach(function(item){
+        console.log(item)
+
+    if(item.name == 'Enviar procuração farmácia de alto custo'){
+      console.log("xxx")
+    }
+  })
+  
+  if(phaseId == "314055677"){
     
      console.log("[Pipe Juridico] "+info.action+" ("+info.card.title+") de "+info.from.name+" para "+info.to.name+" por "+info.moved_by.name+" - "+date)
   
