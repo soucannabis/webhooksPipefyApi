6225a30c975fcbacf84f7d47a998c9c04f566f19
soucanabisdev <118570727+soucanabisdev@users.noreply.github.com>
2023-02-14 10:45:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -240,7 +240,7 @@ app.post('/pipe-juridico', async (req, res) => {
   
   console.log(check)
 
-  if(phaseId == "314055677" && check != '[]'{
+  if(phaseId == "314055677" && check != '[]'){
      console.log("[Pipe Juridico] "+info.action+" ("+info.card.title+") de "+info.from.name+" para "+info.to.name+" por "+info.moved_by.name+" - "+date)
     
      const options = {
