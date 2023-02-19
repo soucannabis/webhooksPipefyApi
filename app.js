8ef4665ca7be9f732afee6c9eaaec1d29f30be2d
soucannabisdev <webmaster@soucannabis.ong.br>
2023-02-19 19:43:05 -0300

    deploy

--- app.js
+++ app.js
@@ -13,7 +13,34 @@ app.use(express.urlencoded({
 
 const pipeAssociados = require("./routes/pipe-associados")
 
-app.use("/", pipeAssociados)
+app.post('/pipe-associados', async (req, res) => {
+  date = new Date()
+ 
+  info = req.body.data
+  api.push(info)
+  
+  phaseId = req.body.data.to.id
+
+  if(phaseId == "316891688"){
+    
+    console.log("[Pipe Associados]"+info.action+" de "+info.from.name+" para "+info.to.name+" por "+info.moved_by.name+" - "+date)
+    
+    const options = {
+    method: "POST",
+    headers:{"Content-Type": "application/json"},
+    mode: "cors",
+    data: req.body.data,
+    url: "https://171af439a56ed234aceb6b6de80993ac.m.pipedream.net"
+    }
+
+    await axios(options)
+    .catch(err => console.log(err))
+
+    res.status(200).end()
+  }
+
+})
+
 
 app.post('/pipe-pedidos', async (req, res) => {
 
