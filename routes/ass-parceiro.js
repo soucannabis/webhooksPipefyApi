56da85a9a4d3c9bfce532169c2421fdc2be4d49b
soucannabisdev <soucannabisadm@gmail.com>
2023-06-27 17:15:43 -0300

    deploy

---
+++ routes/ass-parceiro.js
@@ -0,0 +1,30 @@
+const axios = require('axios')
+const express = require("express")
+const router = express.Router()
+ 
+var date = ""
+
+router.post('/ass-parceiro', async (req, res) => {
+    date = new Date()    
+    
+    if(req.body.name == "Termo de Adesão à Associação Terapêutica - Parceiros"){
+      
+      console.log("[Ass Parceiro] Contrato Assinado por: "+req.body.signers[0].name+" - "+date)
+         
+      const options = {
+      method: "POST",
+      headers:{"Content-Type": "application/json"},
+      mode: "cors",
+      data: req.body,
+      url: "https://eo87f0al6gqai82.m.pipedream.net"
+      }
+    
+      await axios(options)   
+      .catch(err => console.log(err))
+     
+    res.status(200).end()
+      
+    }  
+});
+
+module.exports = router
